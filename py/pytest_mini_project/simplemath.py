ec5e686038e01742fca879360d49891d860954bf
fangshuai03 <fangshuai03@baidu.com>
2024-01-19 18:31:30 +0800

    add docstring for pylint

--- py/pytest_mini_project/simplemath.py
+++ py/pytest_mini_project/simplemath.py
@@ -1,15 +1,23 @@
+#! /usr/bin/env python3
+"""
+simple math functions
+"""
 # simplemath.py
 
 def add(a, b):
+    """add two numbers"""
     return a + b
 
 def subtract(a, b):
+    """subtract two numbers"""
     return a - b
 
 def multiply(a, b):
+    """multiply two numbers"""
     return a * b
 
 def divide(a, b):
+    """divide two numbers"""
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
--- py/pytest_mini_project/test_simplemath.py
+++ py/pytest_mini_project/test_simplemath.py
@@ -1,20 +1,29 @@
+#! /usr/bin/env python3
+"""
+test simple math functions
+"""
 # test_simplemath.py
 
 import pytest
 from pytest_mini_project.simplemath import add, subtract, multiply, divide
 
 def test_add():
+    """test add function"""
     assert add(1, 2) == 3
 
 def test_subtract():
+    """test subtract function"""
     assert subtract(2, 1) == 1
 
 def test_multiply():
+    """test multiply function"""
     assert multiply(2, 3) == 6
 
 def test_divide():
+    """test divide function"""
     assert divide(6, 2) == 3
 
 def test_divide_by_zero():
+    """test divide by zero function"""
     with pytest.raises(ValueError):
         divide(1, 0)
