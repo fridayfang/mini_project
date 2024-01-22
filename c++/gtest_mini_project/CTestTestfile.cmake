f046006baae84f61dda3563e2a7a4c53f431cc19
fs <you@example.com>
2024-01-22 17:44:03 +0800

    make gtest more clean

--- c++/gtest_mini_project/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/fangshuai03/mini_project/c++/gtest_mini_project
-# Build directory: /Users/fangshuai03/mini_project/c++/gtest_mini_project
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
